de87fde0e41bed50ba544e9304857e60ec6acdd0
Arvind Srinivas Subramanian <arvindsrinivas96@gmail.com>
2023-02-20 22:35:07 -0500

    Rev mig1

--- db/migrate/20230216041816_change_credit_card_to_be_string_in_purchases.rb
+++ db/migrate/20230216041816_change_credit_card_to_be_string_in_purchases.rb
@@ -1,5 +1,10 @@
 class ChangeCreditCardToBeStringInPurchases < ActiveRecord::Migration[6.1]
-  def change
+  def up
+    change_column :purchases, :credit_card, :string
+    change_column :purchases, :phone_number, :string
+  end
+
+  def down
     change_column :purchases, :credit_card, :string
     change_column :purchases, :phone_number, :string
   end
